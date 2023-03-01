5210969d699f4d79560a4e33500d2a9f0d4cecdf
jolak5 <kayodej4@gmail.com>
2023-03-01 19:02:58 +0100

    Create add and delete test

--- tests/addTasks.test.js
+++ tests/addTasks.test.js
@@ -1,54 +1,27 @@
-import { deleteTask } from '../modules/listTasks';
+import { addTask } from '../modules/listTasks';
 
-describe('deleting tasks', () => {
-  test('deleteTask', () => {
-    const toDOTasks = [{ task: 'task', completed: false, id: 1 }];
-    const task = { task: 'task', completed: false, id: 1 };
+describe('adding tasks', () => {
+  let toDoTasks = [];
 
-    deleteTask(toDOTasks, task);
-
-    expect(toDOTasks).toEqual([]);
+  beforeEach(() => {
+    toDoTasks = [];
   });
 
-  test('deleting middle task', () => {
-    const toDoTasks = [
-      {
-        task: 'task1',
-        completed: false,
-        id: 1,
-      },
-      {
-        task: 'task2',
-        completed: false,
-        id: 2,
-      },
-      {
-        task: 'task3',
-        completed: false,
-        id: 3,
-      },
-    ];
+  test('add one task', () => {
+    const task = { task: 'task', completed: false, id: 1 };
 
-    const task = {
-      task: 'task2',
-      completed: true,
-      id: 2,
-    };
+    addTask(toDoTasks, 'task');
 
-    deleteTask(toDoTasks, task);
+    expect(toDoTasks).toEqual([task]);
+  });
 
-    expect(toDoTasks).toEqual([
-      {
-        task: 'task1',
-        completed: false,
-        id: 1,
-      },
+  test('add two tasks', () => {
+    const task1 = { task: 'task1', completed: false, id: 1 };
+    const task2 = { task: 'task2', completed: false, id: 2 };
 
-      {
-        task: 'task3',
-        completed: false,
-        id: 2,
-      },
-    ]);
+    addTask(toDoTasks, 'task1');
+    addTask(toDoTasks, 'task2');
+
+    expect(toDoTasks).toEqual([task1, task2]);
   });
 });
