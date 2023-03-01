fc213ec47b0773e64ade33a467869cc79c9ffb0f
jolak5 <kayodej4@gmail.com>
2023-03-01 18:10:42 +0100

    create the add test file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import _ from 'lodash';
 import './style.css';
 
+import { renderToDoList } from '../modules/listTasks.js';
+import toDoTasks from './todoTasks';
+import '../modules/eventListeners.js';
+
 const toDoList = document.querySelector('.todo-list-ul');
 
 renderToDoList(toDoTasks, toDoList);
